98616a0aa32896004fcf29b7d5bdd7bb964a220d
test <test@test.com>
2025-04-28 15:17:10 +0900

    add exception

--- src/main/java/com/example/security_study/config/SecurityConfig.java
+++ src/main/java/com/example/security_study/config/SecurityConfig.java
@@ -4,11 +4,14 @@ import com.example.security_study.filter.JWTAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -28,15 +31,39 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);  // Authorization header Basic 요청을 분석 차단
 
-        http.authorizeHttpRequests(auth -> {
-            auth
-                    .requestMatchers("/member/**").authenticated()  // 해당 pattern은 인증 필요
-                    .requestMatchers("/admin/**").hasAuthority("ADMIN")  // 인증 되어있는 상태에서 + ADMIN이라는 권한 필요
-                    .anyRequest().permitAll();  // 이외의 모든 것은 허용
-        });
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/member/**").authenticated()  // 해당 pattern은 인증 필요
+                            .requestMatchers("/admin/**").hasAuthority("ADMIN")  // 인증 되어있는 상태에서 + ADMIN이라는 권한 필요
+                            .anyRequest().permitAll();  // 이외의 모든 것은 허용
+                })
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint(authenticationEntryPoint());
+                    exception.accessDeniedHandler(accessDeniedHandler());
+                });
+
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+        };
     }
 
     @Bean
